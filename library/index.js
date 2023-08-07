790fb1a70d0c739600d31f0d01f3d1ce33fb0cdd
david-anav <art_migalev@yahoo.com>
2023-08-08 01:04:27 +0300

    feat: " add openburger_menu to js"

--- library/index.js
+++ library/index.js
@@ -1,1 +1,17 @@
 console.log('Вёрстка валидная +10\nВёрстка семантическая +16\nВёрстка соответствует макету +54\nОбщие требования к верстке +20\n')
+
+
+const burgerOpen = document.querySelector('.open_burger_menu');
+const burgerBtn = document.querySelector('.header_humb');
+const burgerClose = document.querySelector('.open_burger_open');
+
+burgerBtn.addEventListener('click',handleclick);
+function handleclick (event) {
+    console.log (burgerOpen.classList);
+    burgerOpen.classList.toggle('openbur');
+}
+burgerClose.addEventListener('click', removeclick);
+function removeclick(event) {
+    burgerOpen.classList.toggle('openbur');
+}
+
